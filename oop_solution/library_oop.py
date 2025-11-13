0f9485cbf8af1093ccefdf9d95f784b56b090338
User_NotExist <49120419+User-NotExist@users.noreply.github.com>
2025-11-13 22:39:34 +0700

    Created Member class

--- oop_solution/library_oop.py
+++ oop_solution/library_oop.py
@@ -31,3 +31,33 @@ class Book:
             return True
 
         return False
+
+
+class Member:
+    def __init__(self, id, name, email):
+        self.id = id
+        self.name = name
+        self.email = email
+        self.borrowed_list = []
+
+    def can_borrow(self):
+        if len(self.borrowed_list) >= 3:
+            return False
+        return True
+
+    def existing_book(self, book_id):
+        if book_id in self.borrowed_list:
+            return True
+        return False
+
+    def borrow_book(self, book_id):
+        if not self.existing_book(book_id) and self.can_borrow():
+            self.borrowed_list.append(book_id)
+            return True
+        return False
+
+    def return_book(self, book_id):
+        if self.existing_book(book_id):
+            self.borrowed_list.remove(book_id)
+            return True
+        return False
